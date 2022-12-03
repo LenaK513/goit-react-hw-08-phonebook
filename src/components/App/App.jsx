cc9228327d547e8161400a7daa18d2cad5ac8420
Olena <kryvtsovaolena@gmail.com>
2022-12-03 18:46:44 +0200

    route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 
 import { FormPage } from 'pages/FormPage/FormPage';
 import { NotFound } from 'pages/NotFound/NotFound';
-
 export const App = () => {
   return (
     <Routes>
