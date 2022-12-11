39821ceaccecbd67e36265d0dcd8a1ef74ed162e
Olena <kryvtsovaolena@gmail.com>
2022-12-11 18:55:48 +0200

    AppBar+Container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,14 @@
+import { AppBar } from 'components/AppBar/AppBar';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export const Container = () => {
-  return <div>Container</div>;
+  return (
+    <div>
+      <AppBar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 };
