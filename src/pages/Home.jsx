7a43c349622080752d31ce92a161a439cdff341b
Olena <kryvtsovaolena@gmail.com>
2022-12-11 17:00:25 +0200

    Register+RegisterForm

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <div>
+      <h2>Connections API 1.0.0 OAS3</h2>
+      Alexander Repeta - Website Servers
+    </div>
+  );
+};
---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
+export const Register = () => {
+  return (
+    <div>
+      <h3>Registration</h3>
+      <RegisterForm />
+    </div>
+  );
+};
