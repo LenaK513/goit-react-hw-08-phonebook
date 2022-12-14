7134546359e4d3c4e50ef94e92edbb7ef3f9b734
Olena <kryvtsovaolena@gmail.com>
2022-12-14 17:25:32 +0200

    PrivateRoute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,10 @@
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
 import { Container } from 'components/Container/Container';
-// import { ContactForm } from 'components/ContactForm/ContactForm';
-// import { ContactList } from 'components/ContactList/ContactList';
-// import { Filter } from 'components/Filter/Filter';
-// import { selectLoading, selectError } from 'redux/contacts/selectors';
+
 import { refreshUser } from 'redux/auth/operations';
 // import { useAuth } from 'hooks';
-// import { Container } from 'components/App/App.styled';
 
 const HomePage = lazy(() => import('../../pages/Home/Home'));
 const RegisterPage = lazy(() => import('../../pages/Register'));
@@ -17,8 +12,6 @@ const LoginPage = lazy(() => import('../../pages/Login'));
 const ContactsPage = lazy(() => import('../../pages/Contacts/Contacts'));
 
 export const App = () => {
-  // const isLoading = useSelector(selectLoading);
-  // const error = useSelector(selectError);
   const dispatch = useDispatch();
   // const { isRefreshing } = useAuth();
 
